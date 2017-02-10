5a0763993dd0319f39a0ae799d8caf4440283f95
Sheng Zha <zhasheng@amazon.com>
2017-02-09 22:24:40 -0800

    sanity test fix

--- test_wheel.sh
+++ test_wheel.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if [[ ! -z $(command -v pip3) ]]; then
-  PIP=pip3
-else
-  PIP=pip
-fi
-$PIP install -U --force-reinstall dist/*.whl
+pip install -U --force-reinstall dist/*.whl
 python sanity_test.py
 
 # @szha: this is a workaround for travis-ci#6522
