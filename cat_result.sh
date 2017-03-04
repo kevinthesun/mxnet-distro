a333a1eda42756614b3804205b2a9ace2204ccc7
Wang <wayao@9801a7a9c287.ant.amazon.com>
2017-03-03 22:56:16 -0800

    Split result cat

---
+++ cat_result.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd /mxnet/tests/nightly
+echo "Test Summary Start"
+sudo cat test_summary.txt
+echo "Test Summary End"
--- notebook_test.sh
+++ notebook_test.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 cd /mxnet/tests/nightly
 sudo python test_ipynb.py
-
-echo "Test Summary Start"
-sudo cat test_summary.txt
-echo "Test Summary End"
